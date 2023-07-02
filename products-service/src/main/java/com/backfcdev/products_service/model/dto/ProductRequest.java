352395b965f75e869f830da0ae41dae3bb06355e
gibbssonfarias30 <gibbfarc17@gmail.com>
2023-07-02 15:16:47 -0500

    DTOs created

---
+++ products-service/src/main/java/com/backfcdev/products_service/model/dto/ProductRequest.java
@@ -0,0 +1,23 @@
+package com.backfcdev.products_service.model.dto;
+
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+@Builder
+public class ProductRequest {
+
+    private String sku;
+
+    private String name;
+
+    private String description;
+
+    private Double price;
+
+    private Boolean status;
+}

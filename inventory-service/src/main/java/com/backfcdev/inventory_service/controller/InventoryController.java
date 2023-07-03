daaf3a2a300a497e5fe10e5bdb72fb6b2552a6fd
gibbssonfarias30 <gibbfarc17@gmail.com>
2023-07-03 11:49:20 -0500

    exposing https requests in the controller

---
+++ inventory-service/src/main/java/com/backfcdev/inventory_service/controller/InventoryController.java
@@ -0,0 +1,31 @@
+package com.backfcdev.inventory_service.controller;
+
+
+import com.backfcdev.inventory_service.model.dto.BaseResponse;
+import com.backfcdev.inventory_service.model.dto.OrderItemRequest;
+import com.backfcdev.inventory_service.model.entities.Inventory;
+import com.backfcdev.inventory_service.service.IInventoryService;
+import lombok.RequiredArgsConstructor;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+
+@RequiredArgsConstructor
+@RestController
+@RequestMapping("/api/v1/inventory")
+public class InventoryController {
+
+    private final IInventoryService inventoryService;
+
+
+    @GetMapping("/{sku}")
+    ResponseEntity<Boolean> isInStock(@PathVariable String sku) {
+        return ResponseEntity.ok(inventoryService.isInStock(sku));
+    }
+
+    @PostMapping("/in-stock")
+    ResponseEntity<BaseResponse> areInStock(@RequestBody List<OrderItemRequest> orderItems) {
+        return ResponseEntity.ok(inventoryService.areInStock(orderItems));
+    }
+}
---
+++ orders-service/src/main/java/com/backfcdev/orders_service/controller/OrderController.java
@@ -0,0 +1,33 @@
+package com.backfcdev.orders_service.controller;
+
+
+import com.backfcdev.orders_service.model.dto.OrderRequest;
+import com.backfcdev.orders_service.model.dto.OrderResponse;
+import com.backfcdev.orders_service.service.IOrderService;
+import lombok.RequiredArgsConstructor;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+
+import static org.springframework.http.HttpStatus.CREATED;
+
+@RequiredArgsConstructor
+@RestController
+@RequestMapping("/api/v1/orders")
+public class OrderController {
+
+    private final IOrderService orderService;
+
+
+    @GetMapping
+    ResponseEntity<List<OrderResponse>> getAllOrders() {
+        return ResponseEntity.ok(orderService.getAllOrders());
+    }
+
+    @PostMapping
+    ResponseEntity<String> placeOrder(@RequestBody OrderRequest orderRequest) {
+        orderService.placeOrder(orderRequest);
+        return ResponseEntity.status(CREATED).body("Order placed successfully!");
+    }
+}

2da11bec3934dbd8dbdcbab025d10411213db0c4
gibbssonfarias30 <gibbfarc17@gmail.com>
2023-07-04 09:01:55 -0500

    enable eureka discovery client

--- orders-service/src/main/java/com/backfcdev/orders_service/OrdersServiceApplication.java
+++ orders-service/src/main/java/com/backfcdev/orders_service/OrdersServiceApplication.java
@@ -2,7 +2,9 @@ package com.backfcdev.orders_service;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 
+@EnableDiscoveryClient
 @SpringBootApplication
 public class OrdersServiceApplication {
 

01b559cc7c8e49e12cdcf04b5b3df18484967199
gibbssonfarias30 <gibbfarc17@gmail.com>
2024-01-14 10:43:23 -0500

    change in method

--- orders-service/src/main/java/com/backfcdev/orders_service/service/IOrderService.java
+++ orders-service/src/main/java/com/backfcdev/orders_service/service/IOrderService.java
@@ -6,7 +6,7 @@ import com.backfcdev.orders_service.model.dto.OrderResponse;
 import java.util.List;
 
 public interface IOrderService {
-    void placeOrder(OrderRequest orderRequest);
+    OrderResponse placeOrder(OrderRequest orderRequest);
 
     List<OrderResponse> getAllOrders();
 }

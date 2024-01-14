8a1066299d21c80534d8da96772327aa7f51a660
gibbssonfarias30 <gibbfarc17@gmail.com>
2024-01-14 10:40:24 -0500

    jsonUtils

---
+++ notification-service/src/main/java/com/backfcdev/notification_service/utils/JsonUtils.java
@@ -0,0 +1,24 @@
+package com.backfcdev.notification_service.utils;
+
+import com.fasterxml.jackson.databind.ObjectMapper;
+
+public class JsonUtils {
+
+    private static final ObjectMapper objectMapper = new ObjectMapper();
+
+    public static String toJson(Object object) {
+        try {
+            return objectMapper.writeValueAsString(object);
+        } catch (Exception e) {
+            throw new RuntimeException();
+        }
+    }
+
+    public static <T> T fromJson(String json, Class<T> clazz) {
+        try {
+            return objectMapper.readValue(json, clazz);
+        } catch (Exception e) {
+            throw new RuntimeException();
+        }
+    }
+}

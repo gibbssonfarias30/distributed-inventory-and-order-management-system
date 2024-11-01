acc4c3c1fc8e860d55b5d47ea759085215b9f339
gibbssonfarias30 <gibbfarc17@gmail.com>
2024-10-31 22:23:28 -0500

    refactor: use bean commandLineRunner

--- inventory-service/src/main/java/com/backfcdev/inventory_service/util/DataLoader.java
+++ inventory-service/src/main/java/com/backfcdev/inventory_service/util/DataLoader.java
@@ -5,6 +5,7 @@ import com.backfcdev.inventory_service.repository.IInventoryRepository;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.boot.CommandLineRunner;
+import org.springframework.context.annotation.Bean;
 import org.springframework.stereotype.Component;
 
 import java.util.List;
@@ -12,23 +13,25 @@ import java.util.List;
 @Component
 @Slf4j
 @RequiredArgsConstructor
-public class DataLoader implements CommandLineRunner {
+public class DataLoader  {
 
     private final IInventoryRepository inventoryRepository;
 
-    @Override
-    public void run(String... args) throws Exception {
-        log.info("Loading data...");
-        if(inventoryRepository.findAll().isEmpty()){
-            inventoryRepository.saveAll(
-                    List.of(
-                            Inventory.builder().sku("00001").quantity(10L).build(),
-                            Inventory.builder().sku("00002").quantity(20L).build(),
-                            Inventory.builder().sku("00003").quantity(30L).build(),
-                            Inventory.builder().sku("00004").quantity(0L).build()
-                    )
-            );
-        }
-        log.info("data loaded...");
+    @Bean
+    CommandLineRunner init() {
+        return args -> {
+            log.info("Loading data...");
+            if(inventoryRepository.findAll().isEmpty()){
+                inventoryRepository.saveAll(
+                        List.of(
+                                Inventory.builder().sku("00001").quantity(10L).build(),
+                                Inventory.builder().sku("00002").quantity(20L).build(),
+                                Inventory.builder().sku("00003").quantity(30L).build(),
+                                Inventory.builder().sku("00004").quantity(0L).build()
+                        )
+                );
+            }
+            log.info("data loaded...");
+        };
     }
 }
